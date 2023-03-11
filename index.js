e05e656b9400f1fa96a47c4bae914e8b8360d9e2
sourav2342 <104992048+sourav2342@users.noreply.github.com>
2023-03-11 15:30:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//generates random text;
 function textGenerator() {
   let result = "";
   let charset = "ABC DEF GHI JKLM NOP QRSTU VWXY Zab  cdefgh ijk lm nopq rstu vwx yz ";
@@ -12,4 +13,43 @@ function textGenerator() {
 
 }
 
-$("p").text(textGenerator());
+let str="",i=0,wrg=0;
+
+// when refresh btn is pressed shiftes the position of blocks using addclass
+$(".ref_btn").click(function(){
+    $(".type_area").addClass("type_shift");
+    $(".ref_btn").addClass("ref");
+    $("#text").addClass("text_diplay");
+    str = textGenerator();
+    $("#text").text(str);
+    wrg=0,i=0;
+});
+
+
+$(".type_area").keypress(function(e){
+
+   if(e.keyCode == 8){
+     i--;
+   }
+
+   if(e.key != str.charAt(i) && e.keyCode != 8 ){
+     wrg++;
+     console.log("pressed wrong key"+ i);
+     i++;
+   }
+
+   if(i==str.length){
+     console.log("Wrong Keys pressed:" +wrg);
+     i=0;
+     $(".type_area").removeClass("type_shift");
+     $(".ref_btn").removeClass("ref");
+     $("#text").removeClass("text_diplay");
+   }
+
+});
+
+
+// removes the text in input tag when a space is pressed
+$(".type_area").keypress(function(e){
+    if(e.keyCode == 32)e.target.value = "";
+});
